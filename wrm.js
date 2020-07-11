b4f83b17fdf0199a873472f77a6814834a795669
Nicholas Blott <blottn@tcd.ie>
2020-07-11 15:41:47 +0100

    created wormholes

--- wrm.js
+++ wrm.js
@@ -1,11 +1,41 @@
-const express = require('express');
-const app = express();
-const expressWs = require('express-ws')(app);
+const path = require('path');
+const http = require('http');
+const ws = require('ws');
 
 const port = 3000;
 
 const roomsURl = '/room/';
 
-app.get('/', (req, res) => res.send('Hello World!'));
+let server = http.createServer((req, res) => {
+    response.writeHead(404);
+    response.end();
+});
 
-app.listen(port, () => {console.log(`Wormholes open on ${port}`)});
+server.listen(port, () => {
+    console.log(`wrm listening on ${port}`);
+});
+
+let wss = new ws.Server({
+    server: server
+});
+
+let rooms = {};
+
+function getRoomNumber(path) {
+}
+
+wss.on('connection', (socket, request) => {
+    if (rooms[request.url] == undefined) {
+        rooms[request.url] = [];
+    }
+    rooms[request.url].push(socket);
+
+    socket.on('close', (code, reason) => {
+        rooms[request.url] = rooms[request.url].filter(s => s != socket);
+    });
+
+    socket.on('message', (data) => {
+        rooms[request.url].filter(s => s != socket)
+            .map(client => client.send(data));
+    });
+});
