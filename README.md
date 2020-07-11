951ec678f4e453e3768b4a1ae3e72e33cf2870f4
Nicholas Blott <blottn@tcd.ie>
2020-07-11 15:47:40 +0100

    made more wrmholey

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# Wrm
+# WRM
 
-Wormhole creator for websockets. Creates rooms and lets anyone join them. Any connection to that room can broadcast to other members.
+Wormhole creator for websockets. Creates wormholes and lets anyone join them. Any connection to that room can broadcast to other members.
 
 ## API
 
-`websocket to -> /room/${room}` - joins a room
-Send jsons
-Receive jsons
+`websocket to -> /${wormhole}` - joins a wormhole 
+
+Send data
+Receive data
+ta-data!
--- wrm.js
+++ wrm.js
@@ -1,10 +1,7 @@
-const path = require('path');
 const http = require('http');
 const ws = require('ws');
 
 const port = 3000;
-
-const roomsURl = '/room/';
 
 let server = http.createServer((req, res) => {
     response.writeHead(404);
@@ -19,23 +16,22 @@ let wss = new ws.Server({
     server: server
 });
 
-let rooms = {};
-
-function getRoomNumber(path) {
-}
+let wrmholes = {};
 
 wss.on('connection', (socket, request) => {
-    if (rooms[request.url] == undefined) {
-        rooms[request.url] = [];
+    if (wrmholes[request.url] == undefined) {
+        wrmholes[request.url] = [];
     }
-    rooms[request.url].push(socket);
+    wrmholes[request.url].push(socket);
 
     socket.on('close', (code, reason) => {
-        rooms[request.url] = rooms[request.url].filter(s => s != socket);
+        wrmholes[request.url] = wrmholes[request.url].filter(
+            s => s != socket
+        );
     });
 
     socket.on('message', (data) => {
-        rooms[request.url].filter(s => s != socket)
+        wrmholes[request.url].filter(s => s != socket)
             .map(client => client.send(data));
     });
 });
