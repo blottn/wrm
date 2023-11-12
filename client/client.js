b6fb86d346071bb2d6d602207be865b4f37be1af
Nicholas Blott <blottn@tcd.ie>
2023-11-12 01:05:01 +0100

    Added peer listening capabilities (untested)

--- client/client.js
+++ client/client.js
@@ -12,16 +12,21 @@ export const connect = (url,
   };
 
   let peers = {}; // Consider local storage persist
+  const peerListeners = {join: [], leave: []};
+
   const destalify = () => { // Remove stale peers
     let now = Date.now();
     console.log('Checking peers:', peers);
     Object.keys(peers)
       .filter(who => (now - peers[who]) > tolerance)
-      .map(who => {
-        delete peers[who]
-      });
+      .filter(who => delete peers[who])
+      .forEach(w =>
+        peerListeners.leave.forEach(f => f(w))
+      );
     console.log('Done with peers:', peers);
     setTimeout(destalify, tolerance);
+    // TODO instead sleep for the amount remaining to the next interval
+    // It could be the listeners took ages so we might need to immediately rerun
   }
   const beat = () => { // Heartbeat
     pushMessage('beat', {});
@@ -31,6 +36,18 @@ export const connect = (url,
   const startPeering = () => {
     destalify();
     beat();
+  }
+
+  const peerOn = (join, leave) => {
+    peerListeners.join.push(join);
+    peerListeners.leave.push(leave);
+  }
+  const peerOff = (f, g) => {
+    let i = peerListeners.join.indexOf(f);
+    peerListeners.join.splice(i, i + 1);
+
+    let j = peerListeners.leave.indexOf(g);
+    peerListeners.leave.splice(j, j + 1);
   }
 
   let close = () => {
@@ -54,15 +71,23 @@ export const connect = (url,
     ws.addEventListener('message', async ({data}) => {
       let { kind, who, content } = JSON.parse(await data.text());
       console.log(`received message`);
+      if (!(who in peers)) {
+        peerListeners.join.forEach(f => f(who));
+      }
       peers[who] = Date.now();
       if (kind === 'msg') {
         listeners.forEach(l => l(content, who));
       }
     });
+
     ws.addEventListener('open', () => {
       startPeering();
       console.log('opened connection');
-      ok({send, close, on, off});
+      ok({send, close, on, off, 'peering': {
+          'on': peerOn,
+          'off': peerOff,
+        }
+      });
     });
     ws.addEventListener('error', () => {
       console.log('there has been an error :S');
--- client/index.js
+++ client/index.js
@@ -1,11 +1,18 @@
 import { connect } from './client.js';
 
 
-let { send, close, on, off } = await connect('ws://localhost:3000');
+let { send, close, on, off, peering} = await connect('ws://localhost:3000');
 console.log(send, close);
 on((m, who) => {
   console.log(`${who} says ${m}`);
 });
+
+peering.on((who) => {
+    console.log(`${who} has connected`);
+  }, (who) => {
+    console.log(`${who} has left`);
+  }
+);
 
 let greet = () => {
   console.log('saying hi');
